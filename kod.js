5f377a79b6ff402b98c12b2162135cb284d3931f
Anka <dhnkjsfbgskj@gmail.com>
2023-03-18 11:49:39 +0100

    v 1.2.5 bulbs change colours on mouse
    Signed-off-by: Anka <dhnkjsfbgskj@gmail.com>

---
+++ kod.js
@@ -0,0 +1,19 @@
+function lightOnViolet(){
+    document.getElementById("bulb1").src="image/bulbvl.png"
+    document.getElementById("bulb2").src="image/bulbbl.png"
+}
+function lightOnBlue(){
+    document.getElementById("bulb2").src="image/bulbbl.png"
+}
+function lightOnGreen(){
+    document.getElementById("bulb3").src="image/bulbgrn.png"
+}
+function lightOnYellow(){
+    document.getElementById("bulb4").src="image/bulbylw.png"
+}
+function lightOff(){
+    document.getElementById("bulb1").src="image/bulboff.png"
+    document.getElementById("bulb2").src="image/bulboff.png"
+    document.getElementById("bulb3").src="image/bulboff.png"
+    document.getElementById("bulb4").src="image/bulboff.png"
+}
